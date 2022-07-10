42b57474eea8652c386837d3fb7d535fddb91762
Mahmoud ALHAMWIEH <102381976+VokanOva@users.noreply.github.com>
2022-07-10 11:37:57 +0300

    Add files via upload
    #One-Dimensional-Array
    #write a java program to sum values of an array.

---
+++ miniPro1/src/mini1.java
@@ -0,0 +1,17 @@
+public class mini1 {
+
+    public static void main(String args[]){
+        int arr[]={5,9,15,27};
+        int sum = 0;
+        for (int i=0; i< arr.length; i++){
+
+        sum +=arr[i];
+        }
+        System.out.println("sum = "+sum);
+
+
+    }
+
+
+
+}
